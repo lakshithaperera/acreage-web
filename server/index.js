0ec04e70a44608fcd19e7be7cb6a7e7d321887c1
lakshithaperera <pasindulakshitha33@gmail.com>
2023-07-04 21:19:12 +0530

    dfgh

--- server/index.js
+++ server/index.js
@@ -3,17 +3,17 @@ const express = require("express");
 const cors = require("cors"); // added
 
 const connectDB = require("./config/MailListdb");
-const connectDBS = require("./config/SurveyFormdb");
+
 
 const app = express();
 
 // routes
 const todo = require("./routes/MailListRoute");
-const survey = require("./routes/SurveyFormRoute");
+
 
 // connect database
 connectDB();
-connectDBS();
+
 
 // cors
 app.use(cors({ origin: true, credentials: true })); // added
@@ -24,7 +24,7 @@ app.get("/", (req, res) => res.send("Server up and running"));
 
 // use routes
 app.use("/api/todo", todo);
-app.use("/api/survey", survey);
+
 
 // setting up port
 const PORT = process.env.PORT || 8000;
