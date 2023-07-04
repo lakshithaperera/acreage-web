125a8a985a3e4be9db0f52534a5682606d1846b9
lakshithaperera <pasindulakshitha33@gmail.com>
2023-07-04 22:01:28 +0530

    gggdhw

--- server/index.js
+++ server/index.js
@@ -3,28 +3,31 @@ const express = require("express");
 const cors = require("cors"); // added
 
 const connectDB = require("./config/MailListdb");
-
+const connectDBS = require("./config/SurveyFormdb");
 
 const app = express();
 
 // routes
 const todo = require("./routes/MailListRoute");
-
+const survey = require("./routes/SurveyFormRoute");
 
 // connect database
 connectDB();
-
+connectDBS();
 
 // cors
-app.use(cors({ origin: true, credentials: true })); // added
+app.use(cors()); // added
 
 // initialize middleware
-app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("Server up and running"));
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.send("API is running..");
+});
 
 // use routes
 app.use("/api/todo", todo);
-
+app.use("/api/survey", survey);
 
 // setting up port
 const PORT = process.env.PORT || 8000;
